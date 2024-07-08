85d266a378f853e7c37100d1583d1f0c4f10b39f
wyj <111860919+wyj-hb@users.noreply.github.com>
2024-07-08 23:54:18 +0800

    Add files via upload

--- 图论/图中点的层次.cpp
+++ 图论/图中点的层次.cpp
@@ -12,7 +12,53 @@ using namespace std;
 /*
 	1
 */
-int main()
-{
-
-}
+//const int N = 100010;
+////使用邻接链表的方法表示图
+//int e[N], ne[N];
+//int idx = 1;
+////h[N]存储根节点
+//int h[N];
+////队列
+//int q[N];
+////dis
+//int dis[N];
+//int n, m;
+////在a作为头结点的链表上插入以b为值的节点
+//void add(int a, int b)
+//{
+//	e[idx] = b; ne[idx] = h[a]; h[a] = idx++;
+//}
+//int bfs()
+//{
+//	//1号节点先入队
+//	q[0] = 1;
+//	int hh = 0, tt = 0;
+//	dis[1] = 0;
+//	while (hh <= tt)
+//	{
+//		int data = q[hh++];
+//		for (int i = h[data]; i !=0; i = ne[i])
+//		{
+//			int j = e[i];
+//			if (dis[j] == -1)
+//			{
+//				dis[j] = dis[data] + 1;
+//				q[++tt] = j;
+//			}
+//		}
+//	}
+//	return dis[n];
+//}
+//int main()
+//{
+//	cin >> n>>m;
+//	int a, b;
+//	memset(dis, -1, sizeof dis);
+//	while (m--)
+//	{
+//		cin >> a >> b;
+//		add(a, b);
+//	}
+//	int data = bfs();
+//	cout << data << endl;
+//}
